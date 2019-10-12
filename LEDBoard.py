7bcbc6405ca650ed896fbaa35cd6f5c656990734
ardus-uk <peter@ardus.co.uk>
2019-10-12 18:08:18 +0100

    Using .items for iterator throuhout

--- LEDBoard.py
+++ LEDBoard.py
@@ -19,50 +19,51 @@ class LEDBoard:
             "Master":21,
         }
         
-        # LEDs keyed by colour
-        self.led_c = {}
+        # LEDs are keyed by colour
+        self.led = {}
         for colour,pin in self.pin_colour.items():
-            self.led_c[colour] = LED(pin)
+            self.led[colour] = LED(pin)
         
-        # Buttons keyed by name
+        # Buttons are keyed by name
         self.button = {}
-        for name in self.pin_button.keys():
-            self.button[name]=Button(self.pin_button[name])
+        for name,pin in self.pin_button.items():
+            self.button[name]=Button(pin)
     
-    def test_led_c(self,colour,duration):
-        self.led_c[colour].on()
+    def test_led(self,colour,duration):
+        self.led[colour].on()
         sleep(duration)
-        self.led_c[colour].off()
+        self.led[colour].off()
 
-    def test_all_leds_c(self,duration,rest_interval):
+    def test_all_leds(self,duration,rest_interval):
         for colour in self.pin_colour.keys():
             print ("Colour: ",colour,"\nDuration: ", duration,"\nRest Interval:",rest_interval)
-            self.test_led_c(colour,duration)
+            self.test_led(colour,duration)
             sleep(rest_interval)
 
     def test_button(self,name):
         print(name,self.button[name].value)
 
-    def test_all_buttons_n(self):
+    def test_all_buttons(self):
         for name in self.pin_button.keys():
             self.test_button(name)
 
 # Self tests
 if __name__ == '__main__':
-    print("LED test program")
+    print("LED and button test program")
     marksboard = LEDBoard()
     print("Testing the Green LED")
-    marksboard.test_led_c("Green",3)
+    marksboard.test_led("Green",3)
     sleep(3)
     print("Testing the Yellow LED")
-    marksboard.test_led_c("Yellow",3)
+    marksboard.test_led("Yellow",3)
     sleep(3)
     print("Testing the Red LED")
-    marksboard.test_led_c("Red",3)
+    marksboard.test_led("Red",3)
     sleep(3)
     print("Testing all LEDs by colour")
-    marksboard.test_all_leds_c(2,1)
+    marksboard.test_all_leds(2,1)
     sleep(3)
-    print("Testing the button")
+    print("Testing the button after one second")
+    sleep(1)
     marksboard.test_button("Master")
     print("All done!")
